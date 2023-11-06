2d2d38f1d1b32684519f88ff5157c0e5d65757f6
takuma <takumaozk0920@gmail.com>
2023-11-06 09:43:22 -0800

    feat: auth login

---
+++ src/pages/login-demo.tsx
@@ -0,0 +1,67 @@
+import { useEffect, useState } from 'react';
+
+const clientId =
+  '658478798216-td0kduh6um8appipkc7psv799d8kmplt.apps.googleusercontent.com';
+const redirectUri = 'http://localhost:3016/login-demo'; // 例として localhost を使用
+
+const axios = require('axios');
+
+async function getUserInfo(accessToken: string) {
+  const url =
+    'https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=' +
+    accessToken;
+
+  try {
+    const response = await axios.get(url);
+    return response.data; // returns user data in JSON format
+  } catch (error) {
+    console.error('Error fetching user info: ', error);
+    throw error;
+  }
+}
+
+function Login() {
+  const [userInfo, setUserInfo] = useState(null); // [1
+  const handleClick = () => {
+    const url = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${clientId}&response_type=token&scope=openid%20profile%20email&redirect_uri=${redirectUri}`;
+    console.log({ url });
+    window.location.href = url;
+  };
+
+  const accessToken =
+    typeof window !== 'undefined' &&
+    window?.location?.hash &&
+    window.location.hash.includes('access_token')
+      ? new URLSearchParams(window.location.hash).get('#access_token')
+      : null;
+
+  useEffect(() => {
+    if (accessToken) {
+      getUserInfo(accessToken).then((userInfo: any) => {
+        console.log(userInfo);
+        setUserInfo(userInfo);
+      });
+    }
+  }, [accessToken]);
+
+  return (
+    <>
+      {userInfo ? (
+        <div>
+          <h2>Logged in!</h2>
+          <p>{userInfo.name}</p>
+          <p>{userInfo.email}</p>
+          <p>{userInfo.picture}</p>
+          <img
+            src="https://lh3.googleusercontent.com/a/ACg8ocLmDoG-TAu0FBZ8yzCP7Rzv_JRiWsMHMicik-t37Glpv4I=s96-c"
+            alt="代替テキスト"
+          />
+        </div>
+      ) : (
+        <button onClick={handleClick}>Google Login</button>
+      )}
+    </>
+  );
+}
+
+export default Login;
