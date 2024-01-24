218999b15499d2dafe49d6a9903276051a190468
takuma <takumaozk0920@gmail.com>
2024-01-24 13:46:53 -0500

    feat: add link at footer

--- src/components/parts/common/Icon/InstagramIcon.tsx
+++ src/components/parts/common/Icon/InstagramIcon.tsx
@@ -1,39 +1,53 @@
-import { color as colors } from '~/styles/utils';
-
-type InstagramIconProps = {
-  color: string;
-};
-const InstagramIcon: React.FC<InstagramIconProps> = ({
-  color = colors.WHITE,
-}) => {
+const InstagramIcon = () => {
   return (
     <svg
-      width="24"
-      height="25"
-      viewBox="0 0 24 25"
-      fill="none"
       xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 48 48"
+      width="35px"
+      height="35px"
     >
+      <radialGradient
+        id="yOrnnhliCrdS2gy~4tD8ma"
+        cx="19.38"
+        cy="42.035"
+        r="44.899"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop offset="0" stopColor="#fd5" />
+        <stop offset=".328" stopColor="#ff543f" />
+        <stop offset=".348" stopColor="#fc5245" />
+        <stop offset=".504" stopColor="#e64771" />
+        <stop offset=".643" stopColor="#d53e91" />
+        <stop offset=".761" stopColor="#cc39a4" />
+        <stop offset=".841" stopColor="#c837ab" />
+      </radialGradient>
       <path
-        d="M17 2.73438H7C4.23858 2.73438 2 4.97295 2 7.73438V17.7344C2 20.4958 4.23858 22.7344 7 22.7344H17C19.7614 22.7344 22 20.4958 22 17.7344V7.73438C22 4.97295 19.7614 2.73438 17 2.73438Z"
-        stroke={color}
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
+        fill="url(#yOrnnhliCrdS2gy~4tD8ma)"
+        d="M34.017,41.99l-20,0.019c-4.4,0.004-8.003-3.592-8.008-7.992l-0.019-20	c-0.004-4.4,3.592-8.003,7.992-8.008l20-0.019c4.4-0.004,8.003,3.592,8.008,7.992l0.019,20	C42.014,38.383,38.417,41.986,34.017,41.99z"
+      />
+      <radialGradient
+        id="yOrnnhliCrdS2gy~4tD8mb"
+        cx="11.786"
+        cy="5.54"
+        r="29.813"
+        gradientTransform="matrix(1 0 0 .6663 0 1.849)"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop offset="0" stopColor="#4168c9" />
+        <stop offset=".999" stopColor="#4168c9" stopOpacity="0" />
+      </radialGradient>
+      <path
+        fill="url(#yOrnnhliCrdS2gy~4tD8mb)"
+        d="M34.017,41.99l-20,0.019c-4.4,0.004-8.003-3.592-8.008-7.992l-0.019-20	c-0.004-4.4,3.592-8.003,7.992-8.008l20-0.019c4.4-0.004,8.003,3.592,8.008,7.992l0.019,20	C42.014,38.383,38.417,41.986,34.017,41.99z"
       />
       <path
-        d="M15.9997 12.1047C16.1231 12.9369 15.981 13.7869 15.5935 14.5337C15.206 15.2805 14.5929 15.8861 13.8413 16.2643C13.0898 16.6426 12.2382 16.7743 11.4075 16.6406C10.5768 16.5069 9.80947 16.1148 9.21455 15.5198C8.61962 14.9249 8.22744 14.1576 8.09377 13.3269C7.96011 12.4962 8.09177 11.6446 8.47003 10.8931C8.84829 10.1415 9.45389 9.52842 10.2007 9.14092C10.9475 8.75342 11.7975 8.61126 12.6297 8.73468C13.4786 8.86056 14.2646 9.25614 14.8714 9.86298C15.4782 10.4698 15.8738 11.2558 15.9997 12.1047Z"
-        stroke={color}
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
+        fill="#fff"
+        d="M24,31c-3.859,0-7-3.14-7-7s3.141-7,7-7s7,3.14,7,7S27.859,31,24,31z M24,19c-2.757,0-5,2.243-5,5	s2.243,5,5,5s5-2.243,5-5S26.757,19,24,19z"
       />
+      <circle cx="31.5" cy="16.5" r="1.5" fill="#fff" />
       <path
-        d="M17.5 7.23438H17.51"
-        stroke={color}
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
+        fill="#fff"
+        d="M30,37H18c-3.859,0-7-3.14-7-7V18c0-3.86,3.141-7,7-7h12c3.859,0,7,3.14,7,7v12	C37,33.86,33.859,37,30,37z M18,13c-2.757,0-5,2.243-5,5v12c0,2.757,2.243,5,5,5h12c2.757,0,5-2.243,5-5V18c0-2.757-2.243-5-5-5H18z"
       />
     </svg>
   );
--- src/constants/router-path.ts
+++ src/constants/router-path.ts
@@ -6,4 +6,5 @@ export const ROUTER_PATH = {
   SUPPORT: '/support',
   TWITTER: 'https://x.com/ku____maaa?s=21',
   INSTAGRAM: 'https://www.instagram.com/takuma.wtnb0920/',
+  LINKEDIN: 'https://www.linkedin.com/in/takuma-watanabe/',
 };
